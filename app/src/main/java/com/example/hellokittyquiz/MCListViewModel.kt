5b1eedc0b6ac395e98b569deaf32ad70746a0278
Austin Legatt <90079811+Austin-Legatt@users.noreply.github.com>
2022-03-27 23:31:27 -0500

    Revert "Revert "Test""

--- app/src/main/java/com/example/hellokittyquiz/MCListViewModel.kt
+++ app/src/main/java/com/example/hellokittyquiz/MCListViewModel.kt
@@ -1,11 +1,16 @@
 package com.example.hellokittyquiz
 
 import androidx.lifecycle.ViewModel
+import java.util.ArrayList
 
 class MCListViewModel: ViewModel() {
     val mcQuests = mutableListOf<MCQuestion>()
-
     init{
         mcQuests += MCQuestion(questText = "What is Hello Kitty's Astrology Sign?", questAnswers = arrayOf("Scorpio", "Gemini", "Pisces", "Taurus"), correctAnswer = 0)
+        mcQuests += MCQuestion(questText = "What is Hello Kitty's last name?", questAnswers = arrayOf("Red", "Blue", "White", "Pink"), correctAnswer = 2)
+        mcQuests += MCQuestion(questText = "How Many Actors Have Voice Acted as Hello Kitty?", questAnswers = arrayOf("1", "3", "5", "7"), correctAnswer = 1)
+        mcQuests += MCQuestion(questText = "What Animal is Hello Kitty's Friend Cathy?", questAnswers = arrayOf("Bunny", "Cat", "Mouse", "Elephant"), correctAnswer = 0)
     }
+
+
 }
